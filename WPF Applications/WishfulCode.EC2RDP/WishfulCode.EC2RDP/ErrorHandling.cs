b697dbfd40fc8167442daa09993ec8ed95f4d09e
Peter Miller <peter.miller@condenast.co.uk>
2011-05-12 19:14:13 +0100

    Rudimentary Error Handling

---
+++ WPF Applications/WishfulCode.EC2RDP/WishfulCode.EC2RDP/ErrorHandling.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace WishfulCode.EC2RDP
+{
+    public static class ErrorHandling
+    {
+        public static void LogError(Exception ex, string key)
+        {
+            //TODO: better error handling
+            Trace.WriteLine(String.Concat("[App Error] ",key,": ", ex.Message));
+        }
+    }
+}
