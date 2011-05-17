adf2b1d7e611a1b2d9cd37372afc1a125b9f545c
Pete <Pete@Pete-AirPC>
2011-05-17 22:39:33 +0100

    Add Glass background to main window when no connections are open. Intention is for this pane to contain a grid of all connections.

--- WPF Applications/WishfulCode.EC2RDP/WishfulCode.EC2RDP/MainWindow.xaml.cs
+++ WPF Applications/WishfulCode.EC2RDP/WishfulCode.EC2RDP/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WishfulCode.EC2RDP.View;
 using WishfulCode.EC2RDP.ViewModel;
+using System.Windows.Interop;
 
 namespace WishfulCode.EC2RDP
 {
@@ -24,8 +25,72 @@ namespace WishfulCode.EC2RDP
         public MainWindow()
         {
             InitializeComponent();
-
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            this.SizeChanged += new SizeChangedEventHandler(MainWindow_SizeChanged);
             this.WindowState = WindowState.Maximized;
+        }
+
+        void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ShowGlass();
+        }
+
+        void ShowGlass()
+        {
+
+            bool showFull = (ConnectionViews.Items.Count == 0);
+             try
+            {
+                // Obtain the window handle for WPF application
+                IntPtr mainWindowPtr = new WindowInteropHelper(this).Handle;
+                HwndSource mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
+                mainWindowSrc.CompositionTarget.BackgroundColor = Color.FromArgb(0, 0,0,0);
+
+                // Get System Dpi
+                System.Drawing.Graphics desktop = System.Drawing.Graphics.FromHwnd(mainWindowPtr);
+                float DesktopDpiX = desktop.DpiX;
+                float DesktopDpiY = desktop.DpiY;
+
+                // Set Margins
+                NonClientRegionAPI.MARGINS margins = new NonClientRegionAPI.MARGINS();
+
+                // Extend glass frame into client area
+                // Note that the default desktop Dpi is 96dpi. The  margins are
+                // adjusted for the system Dpi.
+                if (showFull)
+                {
+                    margins.cxLeftWidth = -1;
+                    margins.cxRightWidth = -1;
+                    margins.cyTopHeight = -1;
+                    margins.cyBottomHeight = -1;
+                }
+                else
+                {
+                    margins.cxLeftWidth = Convert.ToInt32(((int)ConnectionViews.TranslatePoint(new Point(0,0),this).X) * (DesktopDpiX / 96));
+                    margins.cxRightWidth = 0;
+                    margins.cyTopHeight = 0;// Convert.ToInt32(5 * (DesktopDpiX / 96));
+                    margins.cyBottomHeight = 0;
+                }
+
+                int hr = NonClientRegionAPI.DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
+                //
+                if (hr < 0)
+                {
+                    //DwmExtendFrameIntoClientArea Failed
+                }
+            }
+            // If not Vista, paint background white.
+            catch (DllNotFoundException)
+            {
+                Application.Current.MainWindow.Background = Brushes.White;
+            }
+        }
+
+
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ShowGlass();
         }
 
         private void ConnectionListItem_DoubleClick(object sender, MouseButtonEventArgs mouseButtonEventArgs)
@@ -43,6 +108,7 @@ namespace WishfulCode.EC2RDP
                 //find item in open connections and select it
                 ConnectionViews.SelectedItem = selectedItem;
             }
+            ShowGlass();
         }
 
         private void AccountSettings_Click(object sender, RoutedEventArgs e)
