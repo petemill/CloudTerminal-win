c3cd84663922203f8e4b1fb6b8e73975a0b9e200
Henry Donnelly <henry.donnelly@condenast.co.uk>
2011-05-26 15:02:28 +0100

    fixed close button and app name on main window

--- WPF Applications/WishfulCode.EC2RDP/WishfulCode.EC2RDP/View/Accounts.xaml.cs
+++ WPF Applications/WishfulCode.EC2RDP/WishfulCode.EC2RDP/View/Accounts.xaml.cs
@@ -22,5 +22,10 @@ namespace WishfulCode.EC2RDP.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
