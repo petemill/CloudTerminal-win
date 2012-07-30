dca17cb2352e2b9bf6ce616fb0ef69a548c2653e
petemill <petemill@live.com>
2012-07-30 14:11:37 +0100

    Better explain neccessity of private key files for windows administrator password decryption

---
+++ WPF Applications/WishfulCode.EC2RDP/WishfulCode.EC2RDP/Converters/ReverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows;
+
+
+namespace WishfulCode.EC2RDP.Converters
+{
+    public class ReverseBooleanToVisibilityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var boolValue = value as bool?;
+            if (boolValue != null && boolValue.HasValue)
+            {
+                return (boolValue.Value) ? Visibility.Collapsed : Visibility.Visible;
+            }
+            throw new ArgumentException("Not a valid boolean type", "value");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+       {
+           return ((Visibility)value == Visibility.Visible) ? true : false;
+        }
+    }
+}
